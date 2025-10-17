ec2484eedc0a43d3bdb32f27243bf09796e7aca0
lab_services_student <lab_services_student@lab7L95SR>
2025-10-17 13:55:45 +0200

    Add project files.

---
+++ PROG_POE_CMCS/Models/Lecturer.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PROG_POE_CMCS.Models
+{
+    public class Lecturer
+    {
+        [Key]
+        public int LecturerID { get; set; }
+        [Required]
+        public string LecturerName { get; set; }
+
+    }
+}
